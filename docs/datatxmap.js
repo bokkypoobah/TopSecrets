09690e957f6a3f67bf46d61d7bc00ab13eb7a488
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-05-03 16:59:58 +1000

    wip

--- docs/datatxmap.js
+++ docs/datatxmap.js
@@ -2,27 +2,51 @@ async function dataToTx(data, amountUnit, provider) {
   console.log(moment().format("HH:mm:ss") + " dataToTx: " + JSON.stringify(data, bigNumberReplacer, 2));
   let tx = null;
   const errors = {};
+
+  if (data.chainId) {
+    const network = await provider.getNetwork();
+    const chainId = parseInt(network.chainId);
+    if (data.chainId != chainId) {
+      errors.chainId = "Different";
+    }
+  } else {
+    errors.complete = false;
+  }
+
+  let from = null;
+  if (data.from) {
+    try {
+      from = ethers.utils.getAddress(data.from);
+      if (data.nonce) {
+        try {
+          const transactionCount = await provider.getTransactionCount(from);
+          if (data.nonce != transactionCount) {
+            errors.nonce = "Expecting " + transactionCount;
+          }
+        } catch (e) {
+          errors.from = "Error: getTransactionCount()";
+        }
+      } else {
+        errors.complete = false;
+      }
+    } catch (e) {
+      errors.from = "Invalid";
+    }
+  } else {
+    errors.complete = false;
+  }
+  let to = null;
+  if (data.to) {
+    try {
+      to = ethers.utils.getAddress(data.to);
+    } catch (e) {
+      errors.to = "Invalid";
+    }
+  } else {
+    errors.complete = false;
+  }
+
   if (data.action == "ethtransfer") {
-    let from = null;
-    if (data.from) {
-      try {
-        from = ethers.utils.getAddress(data.from);
-      } catch (e) {
-        errors.from = "Invalid";
-      }
-    } else {
-      errors.complete = false;
-    }
-    let to = null;
-    if (data.to) {
-      try {
-        to = ethers.utils.getAddress(data.to);
-      } catch (e) {
-        errors.to = "Invalid";
-      }
-    } else {
-      errors.complete = false;
-    }
     let value = null;
     if (data.amount) {
       try {
@@ -38,14 +62,19 @@ async function dataToTx(data, amountUnit, provider) {
       to,
       value,
       data: null,
+      chainId: data.chainId,
+      nonce: data.nonce,
+      accessList: [],
     };
+
   } else if (data.action == "erc20transfer") {
+    const decimals = 18;
     const contract = new ethers.Contract(data.token, ERC20ABI, provider);
-    const tokens = ethers.utils.parseUnits(data.tokens, 18); // TODO: Decimals
-    // let tokens = null;
+    // const tokens = ethers.utils.parseUnits(data.tokens, 18); // TODO: Decimals
+    let tokens = null;
     if (data.tokens) {
       try {
-        tokens = ethers.utils.parseUnits(data.tokens, amountUnit);
+        tokens = ethers.utils.parseUnits(data.tokens, decimals);
       } catch (e) {
         errors.tokens = "Invalid";
       }
@@ -57,10 +86,31 @@ async function dataToTx(data, amountUnit, provider) {
       from: data.from,
       to: data.token,
       value: 0,
-      data: tokenData,
+      data: tokenData && tokenData.data || null,
+      chainId: data.chainId,
+      nonce: data.nonce,
+      accessList: [],
     };
   } else {
     errors.complete = false;
+  }
+  if (tx) {
+    if (data.gasLimit) {
+      console.log(moment().format("HH:mm:ss") + " dataToTx - tx: " + JSON.stringify(tx, bigNumberReplacer, 2));
+      try {
+        const signer = new ethers.VoidSigner(tx.from, provider);
+        const estimatedGas = await signer.estimateGas(tx);
+        console.log("estimatedGas: " + estimatedGas.toString());
+        if (data.gasLimit < estimatedGas) {
+          errors.gasLimit = "Warning: < estimatedGas: " + estimatedGas;
+        }
+        tx.gasLimit = data.gasLimit;
+      } catch (e) {
+        errors.gasLimit = "Error estimateGas()";
+      }
+    } else {
+      errors.complete = false;
+    }
   }
   return { tx, errors };
 }
